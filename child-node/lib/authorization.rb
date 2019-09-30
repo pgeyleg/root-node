3c17d30a131ba0ada62be387fb6d12acfda04185
Pema Geyleg <pema.geyleg@gmail.com>
2019-09-30 11:58:56 -0400

    Uses Access Management from child-node

---
+++ child-node/lib/authorization.rb
@@ -0,0 +1,1 @@
+Supports Access Management using  policies and roles/groups
---
+++ lib/authorization.rb
@@ -0,0 +1,4 @@
+#Access Management Module
+#Consumes Access Management Library from child node
+
+require 'child-node/lib/authorization'
